22325b0be782fa4b51168835356d01852c691874
Zero445 <victor1414x@gmail.com>
2022-12-10 22:49:32 -0300

    minor size fixes

--- config.h
+++ config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "monospace:pixelsize=15:antialias=true:autohint=true";
-static char *font2[] = {"IPAGothic:pixelsize=15:antialias=true:autohint=true"};
+static char *font = "monospace:pixelsize=13:antialias=true:autohint=true";
+static char *font2[] = {"IPAGothic:pixelsize=13:antialias=true:autohint=true"};
 /* Spare fonts */
 
 static int borderpx = 2;
